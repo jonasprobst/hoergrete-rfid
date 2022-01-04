45093905ab0bb9485f1eb6a1a796d1ba7a6eb662
Jonas Probst <probst.jonas@gmail.com>
2022-01-04 22:05:33 +0100

    whatever

--- test_read_rfid.py
+++ test_read_rfid.py
@@ -5,11 +5,13 @@ from mfrc522 import SimpleMFRC522
 from subprocess import Popen
 
 reader = SimpleMFRC522()
+lastCardId = 0
 
 try:
     while True:
         cardId = reader.read()
-        p = Popen(["echo", "Card ID: " + str(cardId)])
+        if cardId != lastCardId:
+            p = Popen(["echo", "Card ID: " + str(cardId)])
         
 finally:
     GPIO.cleanup()
