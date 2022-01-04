ebd39a58350628f3378c03d5bd20389e0802954f
Jonas Probst <probst.jonas@gmail.com>
2022-01-05 00:02:13 +0100

    ok, last one

--- test_spell_rfid.py
+++ test_spell_rfid.py
@@ -12,6 +12,7 @@ rfidCards = {"0":"n/a"}
 
 try:
     p = Popen(["espeak", "-ven-wm+f2", "-a15", "'ello duck, I'm ready!'", "2>/dev/null"])
+    p.wait()
     while True:
         id, text = reader.read()
         if id != lastId:
@@ -20,13 +21,14 @@ try:
                 p = Popen(["echo", "Card ID: " + str(id)])
             else:
                 rfidCards.update({id:"some URI"})
-                textToSpeak = num2words(id)
-                p = Popen(["espeak", "-ven-wm+f2", "-a15", "sorry me duck, I've never seen this card before", "2>/dev/null"])
-                p = Popen(["espeak", "-ven-wm+f2", "-a15", "but don't you worry lovely. I'll read its id for you", "2>/dev/null"])
-                p = Popen(["espeak", "-ven-wm+f2", "-a15", "you better get a pen and paper", "2>/dev/null"])
-                p = Popen(["espeak", "-ven-wm+f2", "-a15", "well, are you ready? good!", "2>/dev/null"])
+                # spell digits of the id rather than the number
+                textToSpeak = ""
+                for digit in str(id):
+                    textToSpeak += num2words(int(digit)) + ", "
+                p = Popen(["espeak", "-ven-wm+f2", "-a15", "sorry me duck, I've never seen this ID before", "2>/dev/null"])
+                p.wait()
                 p = Popen(["espeak", "-ven-wm+f2", "-a15", "-g25", textToSpeak, "2>/dev/null"])
-                p = Popen(["espeak", "-ven-wm+f2", "-a15", "and remember pet, I won't fall for this twice!", "2>/dev/null"])        
+                p.wait()      
         sleep(5)
 except KeyboardInterrupt:
     raise
