081707e7d911573b4b9f99054511e51d9c73b3a2
Jonas Probst <probst.jonas@gmail.com>
2022-01-05 00:55:27 +0100

    sooo close

--- test_json.py
+++ test_json.py
@@ -3,8 +3,8 @@
 from json import load
 from subprocess import Popen
 
-#print("updating cards.json")
-#p = Popen(["curl", "-s", "-S", "https://raw.githubusercontent.com/jonasprobst/hoergrete-rfid/main/cards.json", ">", "cards.json"])
+print("updating cards.json")
+p = Popen(["curl", "-s", "https://raw.githubusercontent.com/jonasprobst/hoergrete-rfid/main/cards.json$RANDOM", "-o", "cards.json"])
 
 print("reading file")
 with open("cards.json", "r") as file:
@@ -12,10 +12,10 @@ with open("cards.json", "r") as file:
     print(rfidCards)
 
 id = 249056798748
-print("check if id %s is known", str(id))
-if (id in rfidCards.values()):
-    trackUri = rfidCards[id]['uri']
-    trackName = rfidCards[id]['name']
+print("check if id is known:", str(id))
+if (str(id) in rfidCards.values()):
+    trackUri = rfidCards[str(id)]["uri"]
+    trackName = rfidCards[str(id)]["name"]
     print("id found. Track URI: ", trackUri)
     print("id found. Track URI: ", trackName)
 else:
