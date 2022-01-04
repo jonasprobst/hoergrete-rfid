e92eaca26a950fb101e515eb0c98ff86d4cf07f9
Jonas Probst <probst.jonas@gmail.com>
2022-01-04 21:56:32 +0100

    change some things

--- test_read_rfid.py
+++ test_read_rfid.py
@@ -3,7 +3,6 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from subprocess import Popen
-from num2words import num2words
 
 reader = SimpleMFRC522()
 
@@ -11,7 +10,6 @@ try:
     while True:
         cardId = reader.read()
         p = Popen(["echo", "Card ID: " + str(cardId)])
-        textToSpeak = num2words(cardId)
-        p = Popen(["espeak", "-g180", textToSpeak, "2>/dev/null"])
+        
 finally:
     GPIO.cleanup()
--- test_spell_rfid.py
+++ test_spell_rfid.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from subprocess import Popen
+from num2words import num2words
 
 reader = SimpleMFRC522()
 
@@ -10,6 +11,7 @@ try:
     while True:
         cardId = reader.read()
         p = Popen(["echo", "Card ID: " + str(cardId)])
-        
+        textToSpeak = num2words(cardId)
+        p = Popen(["espeak", "-g180", textToSpeak, "2>/dev/null"])
 finally:
     GPIO.cleanup()
