27b007abb4cab3e39577e4c5b4b08b449036f8cd
Jonas Probst <probst.jonas@gmail.com>
2022-01-05 09:05:42 +0100

    some more stuff

--- hoergrete_rfid.py
+++ hoergrete_rfid.py
@@ -7,7 +7,7 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-from subprocess import Popen
+from subprocess import Popen, STDOUT, DEVNULL
 from time import sleep
 from num2words import num2words
 from json import loads
@@ -17,22 +17,20 @@ reader = SimpleMFRC522()
 lastId = 0
 
 def getCards():
+    print("updating cards.json from github...")
     with urllib.request.urlopen("https://raw.githubusercontent.com/jonasprobst/hoergrete-rfid/main/cards.json") as url:
         data = loads(url.read().decode())
+        print(data)
     return data
 
 # do a git pull as startup script
 # or do this after every 
-print("updating cards.json from github...")
 # p = Popen(["curl", "-s", "https://raw.githubusercontent.com/jonasprobst/hoergrete-rfid/main/cards.json", "-o", "cards.json"])
-cards = getCards()
-
-
 #with open("cards.json", "r") as file:
 #    cards = load(file)
+cards = getCards()
 
-p = Popen(["espeak", "-ven-wm+f2", "-a15",
-          "'ello duck, I'm ready for play!'", "2>/dev/null"])
+p = Popen(["espeak", "-ven-wm+f2", "-a15", "ello lovely, let's go!'", "2>/dev/null"], stderr=DEVNULL)
 p.wait()
 
 try:
@@ -42,20 +40,17 @@ try:
             lastId = id
             if (str(id) in cards):
                 trackUri = cards[str(id)]["uri"]
-                trackName = cards[str(id)]["name"]
                 print("Hit play! ID: " + str(id) + " URI: " + trackUri)
-                p = Popen(["espeak", "-ven-wm+f2", "-a15",
-                          str(trackName), "2>/dev/null"])
                 # p = Popen(["mpc", "stop", "-q", "&&",
                 #        "mpc", "clear", "-q", "&&",
                 #        "mpc", "add", str(trackUri), "&&",
                 #        "mpc", "play"])
             else:
                 # spell digits of the id rather than the number
-                textToSpeak = "Oy, a brand new Card I D, "
+                textToSpeak = "New I D!"
                 for digit in str(id):
                     textToSpeak += num2words(int(digit)) + ", "
-                p = Popen(["espeak", "-ven-wm+f2", "-a15", "-g40", textToSpeak, "2>/dev/null"])
+                p = Popen(["espeak", "-ven-wm+f2", "-a15", "-g50", textToSpeak, "2>/dev/null"], stderr=DEVNULL)
                 p.wait()
                 cards = getCards()
         sleep(5)
