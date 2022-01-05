4402f8388484ae369023301236813ae5cd3b22cc
Jonas Probst <probst.jonas@gmail.com>
2022-01-05 20:57:43 +0100

    oookey let's go

--- test_mopidy_control.py
+++ test_mopidy_control.py
@@ -2,10 +2,10 @@
 
 # Alternative (seems EOF tho): https://github.com/ismailof/mopidy-json-client/blob/master/examples/demo_cli.py
 
-from subprocess import Popen
+from subprocess import Popen, DEVNULL
 from time import sleep
 
-def play(uri, rdm="off", sgl="off"):
+def play(uri, rdm="off", sgl="off", vol=0):
     # https://www.systutorials.com/docs/linux/man/1-mpc/
     # other pontentially useful comands: 
     # - load <file> (loads <file> as playlist)
@@ -14,11 +14,13 @@ def play(uri, rdm="off", sgl="off"):
     # - repeat <on|off>
     # - volume [+-]<num> (set the volume to <num> odr adjust it by +/-<num>)
 
-    p = Popen(["mpc", "stop"]).wait()
+    p = Popen(["mpc", "stop"], stdout=DEVNULL).wait()
     p = Popen(["mpc", "clear"]).wait()
     p = Popen(["mpc", "add", str(uri)]).wait()
-    p = Popen(["mpc", "random", str(rdm)]).wait()
-    p = Popen(["mpc", "single", str(sgl)]).wait()
+    if vol != 0:
+        p = Popen(["mpc", "volume", vol], stdout=DEVNULL).wait()
+    p = Popen(["mpc", "random", str(rdm)], stdout=DEVNULL).wait()
+    p = Popen(["mpc", "single", str(sgl)], stdout=DEVNULL).wait()
     p = Popen(["mpc", "play"])
 
 
