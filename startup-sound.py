3360993952e5de99d7548adc2ee5fd46bd13692c
Jonas Probst <probst.jonas@gmail.com>
2022-01-08 21:10:41 +0100

    debug verbose

--- startup-sound.py
+++ startup-sound.py
@@ -11,19 +11,24 @@ from requests import get
 from time import sleep
 
 try:
+    print("looking for mopidy service")
     while True: #wait for mopidy service
         status = call(["systemctl", "is-active", "--quiet", "mopidy"])
         if status == 0:
+            print("looking for mopidy service")
             break
         else:
+            print(...)
             sleep(1)
 
+    print("looking for iris")
     while True :  # if 0 (active), print "Active"
         resp = get("http://localhost:6680/iris")
         if resp.status_code < 400:
             call(["aplay", "-N", "-f", "cd", "/home/pi/hoergrete-rfid/fanfare.wav"])
             exit()
         else:
+            print("...")
             sleep(1)
 finally:
     exit()
