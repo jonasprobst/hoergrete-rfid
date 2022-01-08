e8d9d17ec21818d8e06627509018d2f1ec02076b
Jonas Probst <probst.jonas@gmail.com>
2022-01-08 21:28:26 +0100

    fixes

--- startup-sound.py
+++ startup-sound.py
@@ -23,13 +23,20 @@ try:
 
     print("looking for iris")
     while True :  # if 0 (active), print "Active"
-        resp = get("http://localhost:6680/iris")
+        try:
+            resp = get("http://localhost:6680/iris")
+        except Exception as e:
+            print(e)
+            print("let's keep trying...")
+            sleep(1)
+
         if resp.status_code < 400:
             call(["aplay", "-N", "-f", "cd", "/home/pi/hoergrete-rfid/fanfare.wav"])
             exit()
         else:
-            print("...")
-            sleep(1)
+            print("something's fishy - might even be my code :-S")
+            exit()
+
 except Exception as e:
     print(e)
 finally:    
