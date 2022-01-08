ef9996eedffdbf71827c49d8b3aacd67f38f73d2
Jonas Probst <probst.jonas@gmail.com>
2022-01-08 22:51:49 +0100

    spring clean plus game over clause

--- startup-sound.py
+++ startup-sound.py
@@ -2,44 +2,59 @@
 
 ''' sartup sound
 
-Plays a alsa sound when mopidy is fully booted
+Plays a sound when mopidy is fully booted.
+This script will give up after 5min.
+(I know it's a nasty bit of code :-S)
+
+To run this on boot:
+* sudo nano /etc/rc.local
+* add the following line before "EXIT":
+    * sudo python3 /home/pi/hoergrete-rfid/startup-sound.py &
+* make script executable: sudo chmod +x /etc/rc.local
+* reboot and hope for the best
+
 '''
 
 from subprocess import call
-from typing import SupportsRound
 from requests import get
 from time import sleep
 from timeit import default_timer as timer
 
+start = timer()
+
+def sleepOrExit(naptime=1):
+    lap = timer()
+    if lap - start >= 300:
+        # time is up: play game over sound and abandone ship.
+        call(["aplay", "-N", "-f", "cd", "/home/pi/hoergrete-rfid/gameover.wav"])
+        exit()
+    else:
+        sleep(naptime)
+        return
+
 try:
-    start = timer()
-    print("looking for mopidy service")
+    # wait for mopidy service
     while True: #wait for mopidy service
         status = call(["systemctl", "is-active", "--quiet", "mopidy"])
         if status == 0:
-            print("mopidy found!")
             break
         else:
-            print("...")
-            sleep(1)
+            sleepOrExit(1)
 
-    print("looking for iris")
-    while True :  # if 0 (active), print "Active"
+    # Wait for iris webui
+    while True : 
         try:
             resp = get("http://localhost:6680/iris")
             if resp.status_code < 400:
                 call(["aplay", "-N", "-f", "cd", "/home/pi/hoergrete-rfid/fanfare.wav"])
-                stop = timer()
-                print("this took: ")
-                print(stop - start)
-                exit()
+                break
             else:
-                print("something's fishy - might even be my code :-S")
+                print("Error: iris is running but returned status code: ", resp.status_code)
                 exit()
         except Exception as e:
-            print(e)
-            print("let's keep trying...")
-            sleep(5)
+                # Iris isn't running yet, let's keep trying...
+                sleepOrExit(5)
+
 except Exception as e:
     print(e)
 finally:    
